e8d2f50e57e19aa7e50ac4086a6557a36af48f6c
brangdaj001 <brangdaj001@warren.k12.in.us>
2020-02-14 10:37:24 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+answers = function(questionText, choice1, choice2, choice3, choice4, correctChoice) {
+
+    let answer = document.getElementById("buttons");
+
+    let question1 = document.getElementById("choice0");
+    question1.innerText = " What is the name of ";
+
+    let question2 = document.getElementById("choice1");
+    question2.innerText = "hry ffgddgfg";
+
+
+    let question3 = document.getElementById("choice2");
+    question3.innerText = "gfdg gdgf gfdfg";
+
+
+    let question4 = document.getElementById("choice3");
+    question4.innerText = " gfdgf gfdgrs gdfstrg";
+
+
+};
