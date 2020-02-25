3bf1eac8c7447fba933d4a3568be1d54e61d869c
brangdaj001 <brangdaj001@warren.k12.in.us>
2020-02-25 10:59:17 -0500

    ghhh

--- script.js
+++ script.js
@@ -3,73 +3,56 @@ let numRight = 0;
 let correctOne;
 let numQuestions = 0;
 
+let btn0 = document.getElementById('btn0');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
 
-createQuestion = function(question, choice1, choice2, choice3, choice4, correctChoice) {
-
+createQuestion = function(question, btn0, btn1, btn2, btn3, correctChoice) {
     let response = document.getElementById("question");
     response.innerText = question;
 
-    let box1 = document.getElementById("choice1");
-    box1.innerText = choice1;
+    let box1 = document.getElementById("btn0");
+    box1.innerText = btn0;
 
-    let box2 = document.getElementById("choice2");
-    box2.innerText = choice2;
+    let box2 = document.getElementById("btn1");
+    box2.innerText = btn1;
 
-    let box3 = document.getElementById("choice3");
-    box3.innerText = choice3;
+    let box3 = document.getElementById("btn2");
+    box3.innerText = btn2;
 
-    let box4 = document.getElementById("choice4");
-    box4.innerText = choice4;
+    let box4 = document.getElementById("btn3");
+    box4.innerText = btn3;
 
     let correct = document.getElementById("correctChoice");
-
-
+    correctChoice.innerText = btn1;
 };
 
-let Question1 = function() {
-    createQuestion("what is the name of the things sims carry on their head", "Plumbob", " simbob", "diasims","Simolions", "choice1");
-    progress.innerText ="Question 1 of 4";
-    choice1.onclick = Question2 ;
-    choice2.onclick = Question2 ;
-    choice3.onclick = Question2 ;
-    choice4.onclick = Question2 ;
+let Questions = function(){
 
+    btn0.onclick = function(){checkCorrect(this)};
+    btn1.onclick = function(){checkCorrect(this)};
+    btn2.onclick = function(){checkCorrect(this)};
+    btn3.onclick = function(){checkCorrect(this)};
 
+    if (count === 0){
+        createQuestion("what is the name of the things sims carry on their head", "Plumbob", " simbob", "diasims","Simolions", "Plumbob");
+    }else if (count === 1){
+        createQuestion("What year was the first sims released", "2000", " 2002", "2004","2006", "2000");
+    }else if(count === 2){
+        createQuestion("What language do sims speak?", "Simlish", " Spanish", "Danish","English","Simlish");
+    }else{
+        let question = document.getElementById("question");
+        question.innerText = "You got " + numRight + "correct out of " + numQuestions;
+        document.getElementById("buttons").innerHTML = " ";
+    }
 };
 
-let Question2 = function() {
-    createQuestion("What year was the first sims released", "2000", " 2002", "2004","2006", "choice1");
-    choice1.onclick = Question3 ;
-    choice2.onclick = Question3 ;
-    choice3.onclick = Question3 ;
-    choice4.onclick = Question3 ;
-    progress.innerText ="Question 2 of 4";
-
-
+let checkCorrect = function(button){
+    if(button === correctOne){
+        numRight++;
+    }
+    count++;
+    Questions();
 };
-
-let Question3 = function() {
-    createQuestion("What language do sims speak?", "Simlish", " Spanish", "Danish","English","choice1");
-    progress.innerText ="Question 3 of 4";
-
-    choice1.onclick = Question4 ;
-    choice2.onclick = Question4 ;
-    choice3.onclick = Question4 ;
-    choice4.onclick = Question4 ;
-
-};
-
-let Question4 = function() {
-    createQuestion("what type of money do sims use?", "Simcoins", " Sim Dollars", "Simoleons","Sims Bucks", "choice1") ;
-    progress.innerText ="Question 4 of 4";
-
-
-};
- let checkCorrect = function(button){
-     if ( correctOne === choice3){
-         numRight++
-         createQuestion(" Your score is");
-     } else
-         numRight
- };
 
