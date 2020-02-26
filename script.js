b705ded505916b3e039c3f7316030bef3821d29a
brangdaj001 <brangdaj001@warren.k12.in.us>
2020-02-26 10:39:24 -0500

    ghhhghhhhhhh

--- script.js
+++ script.js
@@ -8,25 +8,32 @@ let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
 let btn3 = document.getElementById('btn3');
 
-createQuestion = function(question, btn0, btn1, btn2, btn3, correctChoice) {
+createQuestion = function(question, choice1, choice2, choice3, choice4, correctChoice) {
     let response = document.getElementById("question");
     response.innerText = question;
 
-    let box1 = document.getElementById("btn0");
-    box1.innerText = btn0;
+    let box1 = document.getElementById("choice1");
+    box1.innerText = choice1;
 
-    let box2 = document.getElementById("btn1");
-    box2.innerText = btn1;
+    let box2 = document.getElementById("choice2");
+    box2.innerText = choice2;
 
-    let box3 = document.getElementById("btn2");
-    box3.innerText = btn2;
+    let box3 = document.getElementById("choice3");
+    box3.innerText = choice3;
 
-    let box4 = document.getElementById("btn3");
-    box4.innerText = btn3;
+    let box4 = document.getElementById("choice4");
+    box4.innerText = choice4;
 
-    let correct = document.getElementById("correctChoice");
-    correctChoice.innerText = btn1;
-    
+    if(correctChoice === choice1){
+        correctOne = btn0;
+    }else if(correctChoice === choice2){
+        correctOne = btn1;
+    }else if(correctChoice === choice3){
+        correctOne = btn2;
+    }else if(correctChoice === choice4){
+        correctOne = btn3;
+    }
+
 };
 
 let Questions = function(){
@@ -42,12 +49,15 @@ let Questions = function(){
         createQuestion("What year was the first sims released", "2000", " 2002", "2004","2006", "2000");
     }else if(count === 2){
         createQuestion("What language do sims speak?", "Simlish", " Spanish", "Danish","English","Simlish");
+    }else if( count === 3){
+        createQuestion("What sims games are there?", "4", " 1", "2","3","4");
     }else{
         let question = document.getElementById("question");
         question.innerText = "You got " + numRight + " correct out of " + numQuestions;
         document.getElementById("buttons").innerHTML = " ";
-    }
 
+    }
+    document.getElementById("progress").innerText = "Question " + (count+ 1) + " Of " + numQuestions;
 };
 
 let checkCorrect = function(button){
